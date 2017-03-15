a6aa58bb7fc3b7a8946a076fdebc4376c402f7a8
Pujita Tipnis <pujita.tipnis@gmail.com>
2017-03-15 19:43:56 -0400

    Beautification - version 1

---
+++ Greendurance/TranspModalViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TranspModalViewController.swift
+//  Greendurance
+//
+//  Created by Pujita Tipnis on 3/14/17.
+//  Copyright © 2017 Pujita Tipnis. All rights reserved.
+//
+
+import UIKit
+
+class TranspModalViewController: UIViewController {
+
+    @IBOutlet weak var textLabel: UILabel!
+    
+    let help = "This section contains tasks related to eco-friendly transportation choices. Select a task for the day and on completion, you would get awarded the assigned points."
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        textLabel.text = help
+    }
+
+    @IBAction func closeTapped(_ sender: Any) {
+        // Dismiss the view controller (the first screen) that
+        // was presented modally by the view controller
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ Greendurance/TransportationPageViewController.swift
@@ -0,0 +1,49 @@
+//
+//  TransportationPageViewController.swift
+//  Greendurance
+//
+//  Created by Pujita Tipnis on 3/14/17.
+//  Copyright © 2017 Pujita Tipnis. All rights reserved.
+//
+
+import UIKit
+
+class TransportationPageViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var searchText = ""
+    var transportTask : [String] = ["Walk to work", "Take the metro", "Carpool with friends", "Use bikesharing today", "Take the bus"]
+    var points : [String] = ["10", "7", "5", "10", "7"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return transportTask.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "report") ?? UITableViewCell(style: .subtitle, reuseIdentifier: "report")
+        
+        cell.detailTextLabel?.text = ""
+        let task = transportTask[indexPath.row]
+
+        cell.textLabel!.text = task
+        cell.detailTextLabel?.text = "Earnable points: \(points[indexPath.row])"
+        //cell.imageView?.image = UIImage(data: "icon.png" as! Data)
+        
+        return cell
+    }
+    
+    @IBAction func helpTapped(_ sender: Any) {
+        print("Help tapped")
+    }
+}
